d95192d3ca11046a9fa736ec9f816fde219ea367
Fernanda Luna <ferluna.rm@gmail.com>
2023-08-27 19:06:51 -0600

    Add blog page

---
+++ src/app/blog/page.js
@@ -0,0 +1,12 @@
+const Blog = () => {
+	return (
+		<div
+			id='blog'
+			className='container mx-auto w-full md:max-w-prose'>
+			<h1>Blog</h1>
+			<section>Blog</section>
+		</div>
+	);
+};
+
+export default Blog;
