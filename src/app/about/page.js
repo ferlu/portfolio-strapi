0cc1f88c86d41d28479e1edb4f3cc0eb0702bc26
Fernanda Luna <ferluna.rm@gmail.com>
2023-08-28 18:16:22 -0600

    * Complete about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 const About = () => {
 	return (
 		<div
@@ -7,7 +8,68 @@ const About = () => {
 				About
 			</h1>
 			<section id='about-content'>
-				<p className=''>Hey there, I&apos;m Fer, a frontend dev from MX. </p>
+				<div>
+					<p className=''>
+						Hi there, I'm Fer, a frontend developer from the beautiful city of
+						Guadalajara, Mexico.
+					</p>
+					<p>
+						I am passionate about frontend technologies, such as{" "}
+						<span className='text-primary'>
+							React, Next.JS, JS and TypeScript.{" "}
+						</span>
+						I am also interested in mobile development and DevOps (although I
+						have 0 experience).
+					</p>
+					<br />
+					<p>
+						I like frontend because, for me, it's pure fun, and creating a good
+						UI is ✨😫🤌 *chef's kiss*{" "}
+						<span className='text-primary'>SO satisfying.</span> My professional
+						goal is become a team lead, and eventually a good manager, or even a
+						frontend architect one day. I really like this stuff and honestly I
+						see myself coding in 10 years.
+					</p>
+					<br />
+					<p>
+						I love hanging out with my partner/friends/family, reading, drawing,
+						mechanical keebs, scrolling through pinterest, learning new
+						languages, cats! (I have 4 and I love them!). <br /> Recently I've
+						been making small changes towards a more conscious and minimal
+						lifestyle, which is something I like as well. Reducing
+						overconsumption and overstimulation is such a great thing. Phew,
+						long list <small>(and I'm holding myself back)</small>
+					</p>
+					<br />
+					<p>
+						What else? hmmmm...I've wanted to create my blog for so long! I'm
+						very happy with it. It took me like a whole weekend. Lots of hard
+						work, and silly bugs 🪲.
+					</p>
+					<br />
+					<ul>
+						Finally, two important things:
+						<li className='list-disc'>
+							This is *obviously* a personal blog, and even though it's aimed at
+							work-related issues, there will be personal and subjective
+							content! Don't be weird about it.
+						</li>
+						<li className='list-disc'>
+							I will use Spanish/English for my blog posts at random! I'll also
+							try to slip in some French and (super basic, don't expect much)
+							Japanese posts once in a while.
+						</li>
+					</ul>
+					<br />
+					<p className=' text-lg text-primary font-bold'>
+						{" "}
+						Anyway, that's all! have a great time
+					</p>
+					<br />
+					<code className='text-sm text-accent'>
+						Stack: Next.JS, React, TailwindCSS, Strapi, Apollo, GraphQL.
+					</code>
+				</div>
 			</section>
 		</div>
 	);
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useEffect } from "react";
 import Typed from "typed.js";
 
@@ -24,7 +25,7 @@ export default function Home() {
 			className='max-w-2xl mt-48 font-sans'>
 			<h1 className='text-4xl font-extrabold md:text-4xl'>Hey there.</h1>
 			<h2 className='text-3xl font-bold'>
-				I&apos;m Fer, a{" "}
+				I&apos;m Fer Luna, a{" "}
 				<span
 					className='bg-clip-text text-transparent bg-gradient-to-r from-secondary/80 to-primary/80'
 					ref={el}
@@ -32,6 +33,55 @@ export default function Home() {
 				<br></br> with lots of things to{" "}
 				<span className='line-through italic'>rant</span> write about.
 			</h2>
+			<div className='divider'></div>
+			<div className='flex justify-end'>
+				<a href='https://linkedin.com/in/fernandalu'>
+					<button className='btn btn-square bg-base-200'>
+						<svg
+							class='h-6 w-6 text-primary'
+							viewBox='0 0 24 24'
+							fill='none'
+							stroke='currentColor'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'>
+							<path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z' />
+							<rect
+								x='2'
+								y='9'
+								width='4'
+								height='12'
+							/>
+							<circle
+								cx='4'
+								cy='4'
+								r='2'
+							/>
+						</svg>
+					</button>
+				</a>
+
+				<a href='https://github.com/ferlu'>
+					<button className='btn btn-square bg-base-200'>
+						<svg
+							class='h-6 w-6 text-primary'
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							strokeWidth='2'
+							stroke='currentColor'
+							fill='none'
+							strokeLinecap='round'
+							strokeLinejoin='round'>
+							<path
+								stroke='none'
+								d='M0 0h24v24H0z'
+							/>
+							<path d='M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21' />
+						</svg>
+					</button>
+				</a>
+			</div>
 		</div>
 	);
 }
